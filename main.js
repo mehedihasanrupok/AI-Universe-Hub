e36ae4a3f12eae95d6161aca340f5f6324d67104
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-03-02 23:19:49 +0600

    DataLoads

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const loadAiMethod = async() =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+
+loadAiMethod();
