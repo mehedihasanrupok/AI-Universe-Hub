96a9f4c1dc7030092487b006428beb070d990d06
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-03-04 01:03:53 +0600

    ModalDone

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const displayAiMethod = (methods,dataLimit) =>{
         <small class="d-inline fw-normal"><i class="m-1 fa-solid fa-calendar-days"></i>${method.published_in}</small>
         </div>
         <div>
-        <button class="d-inline fw-bold bg-info rounded-circle"><i class="fa-solid fa-arrow-right"></i></button>
+        <button onclick="loadDetails('${method.id}')" class="d-inline fw-bold bg-info rounded-circle" data-bs-toggle="modal" data-bs-target="#methodDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
         </div>
         </div>
         
@@ -52,15 +52,16 @@ const displayAiMethod = (methods,dataLimit) =>{
     });
     //stop loader
     toggleSpinner(false);
-
 }
 
+//Show All Button
 document.getElementById('btn-show-all').addEventListener('click',function(){
     //start loader
      toggleSpinner(true);
      loadAiMethod(12);
 })
 
+//Spinner Function
 const toggleSpinner = isLoading =>{
   const loaderSection = document.getElementById('loader');
   if(isLoading){
@@ -70,4 +71,96 @@ const toggleSpinner = isLoading =>{
     loaderSection.classList.add('d-none');
   }
 }
+
+//For Details of Each Method
+const loadDetails =async (id) =>{
+  const url= `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayMethodDetails(data.data);
+}
+
+const displayMethodDetails = method =>{
+  const feature_names = [];
+  for (let feature in method.features) {
+      feature_names.push(method.features[feature].feature_name);
+  }
+  const modalTitle = document.getElementById('methodDetailModalLabel');
+  modalTitle.innerText = method.tool_name;
+
+  const methodDetails = document.getElementById('method-details');
+  methodDetails.innerHTML = `
+    <div class="col">
+        <div class="card h-100 bg-danger bg-opacity-10 border-danger">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">${method.description}</h5>
+                <div class="row row-cols-1 row-cols-md-3 g-2 mt-2">
+                    <div class="col">
+                        <div class="card h-100 bg-white border-0 rounded-4">
+                            <div class="card-body px-0">
+                                <p class="fw-semibold text-success text-center">
+                                    <small>
+                                        ${method.pricing ? method.pricing[0].price : 'Free Cost'} <br> 
+                                        ${method.pricing ? method.pricing[0].plan : 'Basic'}
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100 bg-white border-0 rounded-4">
+                            <div class="card-body px-0">
+                                <p class="fw-semibold text-warning text-center">
+                                    <small>
+                                        ${method.pricing ? method.pricing[1].price : 'Free Cost'} <br> 
+                                        ${method.pricing ? method.pricing[1].plan : 'Pro'}
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100 bg-white border-0 rounded-4">
+                            <div class="card-body px-0">
+                                <p class="fw-semibold text-danger text-center">
+                                <small>
+                                    ${method.pricing ? method.pricing[2].price : 'Free Cost'} <br> 
+                                    ${method.pricing ? method.pricing[2].plan : 'Enterprise'}
+                                </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mt-3">
+                    <div>
+                        <h5 class="card-title fw-bold">Features</h5>
+                        <p>${feature_names.map(feature => `<li class="text-dark-emphasis"><small>${feature}</small></li>`).join('')}</p>
+                    </div>
+                    <div>
+                        <h5 class="card-title fw-bold">Integrations</h5>
+                        <p>${method.integrations ? method.integrations.map(integration => `<li class="text-dark-emphasis">
+                        <small>${integration}</small></li>`).join('') : `<small class="text-dark-emphasis">No data Found</small>`}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card h-100">
+            <img src="${method.image_link[0]}" class="m-3 rounded-3" alt="...">
+            <button type="button" class="btn btn-danger btn-sm">${method.accuracy.score * 100}&percnt; accuracy</button>
+            <div class="card-body h-25">
+                <h5 class="card-title text-center fw-bold">
+                    ${method.input_output_examples ? method.input_output_examples[0].input : 'Can you give any example?'}
+                </h5>
+                <p class="card-text text-center fw-light">
+                    ${method.input_output_examples ? method.input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}
+                </p>
+            </div>
+        </div>
+    </div>
+    `;
+}
 loadAiMethod();
+
